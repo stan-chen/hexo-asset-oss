eca80004c54bd8aa657263869cf91b69a35b5e02
StanleyChen <chenxuefei_pp@163.com>
2020-03-11 19:30:21 +0800

    Add eslint and prettier.

--- index.js
+++ index.js
@@ -7,30 +7,42 @@
 'use strict';
 const url = require('url');
 const pathFn = require('path');
+// eslint-disable-next-line no-undef
 const ctx = hexo;
 const oss_config = ctx.config.asset_oss;
-const enable = oss_config.enable &&
-    oss_config.oss_url.length &&
-    oss_config.oss_acid.length &&
-    oss_config.oss_ackey.length &&
-    oss_config.oss_region.length &&
-    oss_config.oss_bucket.length;
+const enable =
+  oss_config.enable &&
+  oss_config.oss_url.length &&
+  oss_config.oss_acid.length &&
+  oss_config.oss_ackey.length &&
+  oss_config.oss_region.length &&
+  oss_config.oss_bucket.length;
 
-if(enable) {
-    hexo.extend.tag.register('ossimg', function(args) {
-        const PostAsset = ctx.model('PostAsset');
-        let imageRoot = ctx.config.root;
-        if (oss_config.oss_root) {
-            imageRoot = pathFn.join(oss_config.oss_root, "post-assets/");
-            imageRoot = url.resolve(oss_config.oss_url, imageRoot);
-        }
-        const slug = args.shift();
-        if (!slug) return;
-        const asset = PostAsset.findOne({post: this._id, slug: slug});
-        if (!asset) return;
-        const title = args.length ? args.join(' ') : '';
-        const alt = title || asset.slug;
-        return '<img src="' + url.resolve(imageRoot, asset.path) + '" alt="' + alt + '" title="' + title + '">';
-    });
-    hexo.extend.filter.register('after_generate', require('./lib/process')(ctx) );
+if (enable) {
+  // eslint-disable-next-line no-undef
+  hexo.extend.tag.register('ossimg', function(args) {
+    const PostAsset = ctx.model('PostAsset');
+    let imageRoot = ctx.config.root;
+    if (oss_config.oss_root) {
+      imageRoot = pathFn.join(oss_config.oss_root, 'post-assets/');
+      imageRoot = url.resolve(oss_config.oss_url, imageRoot);
+    }
+    const slug = args.shift();
+    if (!slug) return;
+    const asset = PostAsset.findOne({ post: this._id, slug: slug });
+    if (!asset) return;
+    const title = args.length ? args.join(' ') : '';
+    const alt = title || asset.slug;
+    return (
+      '<img src="' +
+      url.resolve(imageRoot, asset.path) +
+      '" alt="' +
+      alt +
+      '" title="' +
+      title +
+      '">'
+    );
+  });
+  // eslint-disable-next-line no-undef
+  hexo.extend.filter.register('after_generate', require('./lib/process')(ctx));
 }
